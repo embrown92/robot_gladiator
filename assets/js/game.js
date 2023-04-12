188abad02d20339ea1da5a408336eca2d5c8c281
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2023-04-12 15:44:28 -0400

    update shop function to accept integers for making a selection

--- assets/js/game.js
+++ assets/js/game.js
@@ -240,29 +240,28 @@ var endGame = function () {
 var shop = function () {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE' or 'LEAVE' to make a choice. "
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter: '1' to REFILL, '2' to UPGRADE or '3' to LEAVE. "
     );
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
 
     // use switch case to carry out action
     switch (shopOptionPrompt) {
 
         // if player picks refill and has money, add 20 to playerHealth and subtract 7 from playerMoney
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
         // if player picks UPGRADE and has money, add 6 to playerAttack and subtract 7 from playerMoney
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
         // if player picks LEAVE, exit the store
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
-
             // do nothing, so function will end
             break;
 
